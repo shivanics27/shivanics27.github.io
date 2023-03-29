3641e4be4785d87a7b08778ff984bf94db1219ca
shivanics27 <119922856+shivanics27@users.noreply.github.com>
2023-03-29 10:06:01 +0530

    Add files via upload

---
+++ todo/index.js
@@ -0,0 +1,80 @@
+const todoInput = document.getElementById('todoInput')
+const todoAdd = document.getElementById('todoAdd')
+const todoList = document.getElementById('todoList')
+
+todoAdd.onclick = function(){
+  if(todoInput.value == '')
+    return;
+  
+  while(todoInput.value[0] == ' '){
+    todoInput.value = todoInput.value.substring(1,todoInput.value.length)
+  }
+  
+  while(todoInput.value[todoInput.value.length] == ' '){
+    todoInput.value = todoInput.value.substring(0,todoInput.value.length - 1)
+  }
+  
+  const li = document.createElement('li')
+  li.setAttribute('class','pending')
+  const checkbox = document.createElement('img')
+  checkbox.setAttribute('class','unchecked')
+  checkbox.src = 'images/unchecked.png'
+  const span = document.createElement('span')
+  span.setAttribute('class','unfinished')
+  const removeBtn = document.createElement('img')
+  removeBtn.setAttribute('class','removeItem')
+  removeBtn.src = 'images/remove.png'
+  
+  span.textContent = todoInput.value
+  todoInput.value = ''
+  
+  li.appendChild(checkbox)
+  li.appendChild(span)
+  li.appendChild(removeBtn)
+  todoList.appendChild(li)
+}
+
+todoList.addEventListener('click',(e)=>{
+  if(e.target.className == 'removeItem'){
+    e.target.parentElement.remove()
+    return
+  }
+
+  if(e.target.parentElement.tagName != 'LI'){return}
+  
+  const item = e.target.parentElement
+  
+  if(item.firstElementChild.className == 'checked'){
+    item.setAttribute('class','unfinished')
+    item.firstElementChild.src = 'images/unchecked.png'
+    item.firstElementChild.setAttribute('class','unchecked')
+  } else {
+    item.setAttribute('class','finished')
+    item.firstElementChild.src = 'images/checked.png'
+    item.firstElementChild.setAttribute('class','checked')
+  }
+})
+
+todoList.addEventListener('mouseover',(e)=>{
+  if(e.target.className == 'removeItem'){
+    e.target.src = 'images/remove hover.png'
+  }
+})
+
+todoList.addEventListener('mouseout',(e)=>{
+  if(e.target.className == 'removeItem'){
+    e.target.src = 'images/remove.png'
+  }
+})
+
+function save(){
+    localStorage.setItem('content',document.getElementById('todoList').innerHTML)
+}
+
+function read(){
+    document.getElementById('todoList').innerHTML = localStorage.getItem('content')
+}
+
+let saver = window.setInterval(save,2000)
+
+window.onload = function(){read()}
